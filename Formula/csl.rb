f12f22e85a684f3568631c9b3da4ad001431b805
Ben Chen <bn@bitbrew.dev>
2026-07-19 00:06:23 +0800

    chore(Formula): bump csl to 1.14.0 (#94)
    Update the csl formula to version 1.14.0 and refresh the SHA256
    checksums for all platform-specific binaries (darwin arm64/amd64, linux
    arm64/amd64).

--- Formula/csl.rb
+++ Formula/csl.rb
@@ -8,26 +8,26 @@ require_relative "../scripts/github_prv_repo_download_strategy"
 class Csl < Formula
   desc "Consilium CLI for development workflows"
   homepage "https://github.com/benbenbang/consilium"
-  version "1.13.0"
+  version "1.14.0"
   license "Proprietary"
 
   # Platform-specific URLs using the custom download strategy
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/benbenbang/consilium/releases/download/#{version}/csl-darwin-arm64",
         using: GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "a07c6239aad3476b9639045a51653f7efcd8fdd1c349a3da8e6b7279a8e3c947"
+    sha256 "fd693abb562feaf9b549070dd0b816c1b98c57f23e76f8bde988e8c6da1a5c79"
   elsif OS.mac? && Hardware::CPU.intel?
     url "https://github.com/benbenbang/consilium/releases/download/#{version}/csl-darwin-amd64",
         using: GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "149815ab51e6f01ba2fdc29282fa3f7b551a2271c43c25241560c6cb920fbe02"
+    sha256 "9a36862f2f063686076721c05b7c764bb3ed5d7ae1e3764f613670ca3b0db826"
   elsif OS.linux? && Hardware::CPU.arm?
     url "https://github.com/benbenbang/consilium/releases/download/#{version}/csl-linux-arm64",
         using: GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "896d1523f7427a880235133d17134f08380c26bf0bfa067ef988204f045ee6d1"
+    sha256 "2ff57e9f1ddc87886a98817ee1359978ae0b946e61ba5990c709f7199f318adf"
   elsif OS.linux? && Hardware::CPU.intel?
     url "https://github.com/benbenbang/consilium/releases/download/#{version}/csl-linux-amd64",
         using: GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "b298f4c98b9e8e4eedfe39e693afcc5c82986aad716da35fe18f45a019c96077"
+    sha256 "1ca11c9c28685b4b67710586dd8362282c14f430134ee96922a0e4c9d340dbd3"
   end
 
   def install
