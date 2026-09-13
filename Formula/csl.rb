8aef8f38c658398c44edeaae76629dc79dbffabf
Ben Chen <bn@bitbrew.dev>
2026-09-13 23:48:55 +0900

    chore(formula): bump csl to version 1.16.0 (#119)
    Update the csl formula to version 1.16.0 and refresh the SHA256
    checksums for all platform-specific binaries (macOS arm64/amd64 and
    Linux arm64/amd64).

--- Formula/csl.rb
+++ Formula/csl.rb
@@ -8,26 +8,26 @@ require_relative "../scripts/github_prv_repo_download_strategy"
 class Csl < Formula
   desc "Consilium CLI for development workflows"
   homepage "https://github.com/benbenbang/consilium"
-  version "1.15.0"
+  version "1.16.0"
   license "Proprietary"
 
   # Platform-specific URLs using the custom download strategy
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/benbenbang/consilium/releases/download/#{version}/csl-darwin-arm64",
         using: GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "f01f9d75aa9fe1dd9cdbf1d5d9e1d8272791868889d44d3237fdc92b7e5669a2"
+    sha256 "8a8c6cc975097043210ab6b98e7dae04878409fbf02891952059e069490da214"
   elsif OS.mac? && Hardware::CPU.intel?
     url "https://github.com/benbenbang/consilium/releases/download/#{version}/csl-darwin-amd64",
         using: GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "0756fb278d8b2f82fc83e00def2dcbed38e7df6c10b9cc3f0af845ff9b13f757"
+    sha256 "6c940761449c924f3fe3fcd31e350f21b35c98e338e69b670470ba35c5539821"
   elsif OS.linux? && Hardware::CPU.arm?
     url "https://github.com/benbenbang/consilium/releases/download/#{version}/csl-linux-arm64",
         using: GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "4835cc24c42811da0bd3aabf00c14c7afffc871600f1d2a7216998f425d6466d"
+    sha256 "a207a3ea0240151ac78b10ca7f84e1be25dd7828351de73f4bdfc56de173d4cf"
   elsif OS.linux? && Hardware::CPU.intel?
     url "https://github.com/benbenbang/consilium/releases/download/#{version}/csl-linux-amd64",
         using: GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "7c8fde09ea90ba6d17cb5d9099b542f8a03510f689c241be85738c9f1f3c48fd"
+    sha256 "915d8a1ef552da9dc7180c8939c5bf59ffab6dd1390eb3c411b00286d1096e84"
   end
 
   def install
